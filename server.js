156c1c3ac7d238e0946b6ed1019e5d4a434c7f59
anbiun <33512505+anbiun@users.noreply.github.com>
2023-11-07 12:56:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const jwt = require('jsonwebtoken'); // เพิ่ม jsonwebtoken
 const bodyParser = require('body-parser');
 const port = 3001;
 const privateKey = 'nitipon1988';
-var indexRouter = require('../routes/index');
-
-const whitelist = [
-  '*'
-];
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
